21594775b31592bb29413123458224753240d0af
Le Phuc <phucleo093@gmail.com>
2020-10-25 08:09:20 +0700

    Add lib Sqlite3

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+let dbclient = require('./lib/SQLiteHelperJS/objects/DatabaseClient');
+let dbObj = require('./lib/SQLiteHelperJS/objects/DataObject');
+dbclient.InitDatabase("phuctest3");
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => 
+  {
+    dbclient.ExecuteQuery("SELECT * FROM users", function(result){
+        console.log(__dirname);
+        if(result.result == true){
+            dbObj = result.dataObject;
+            console.log(dbObj);
+            res.send('Hello Express ' + dbObj.GetRowData(0).username);
+        }
+        // res.send('Hello Express');
+    });
+    // res.render('pages/index')
+  
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
