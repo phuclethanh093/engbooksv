8e4f0d66bb395055bf7563628c609ee8dbe0a5be
Le Phuc <phucleo093@gmail.com>
2020-11-14 08:31:27 +0700

    Change

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,5 @@
 var routerModule = require('./../lib/MViC/components/MVCRouter');
 var baseController = require('./../lib/MViC/objects/BaseController');
-// class home extends  baseController{
-//     constructor(){
-//         super();
-        
-//     }
-
-//     homepage(callback){
-//         render.render("/pages/")
-//         return;
-//     }   
-// }
 
 var home = function() {
     baseController.call(this);
--- index.js
+++ index.js
@@ -2,72 +2,17 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 const mvcCore = require('./lib/MViC/components/MVCCore');
 
-// let dbclient = require('./lib/SQLiteHelperJS/objects/DatabaseClient');
-// let dbObj = require('./lib/SQLiteHelperJS/objects/DataObject');
-// dbclient.InitDatabase("phuctest3");
-
-// mvcCore.app.use(mvcCore.express.static(path.join(__dirname, 'public')));
-// mvcCore.app.set('views', path.join(__dirname, 'views'));
-// mvcCore.app.set('view engine', 'ejs');
-// mvcCore.listMapRoute = {
-//   "/home" : {
-//     controllerName : "home",
-//     action: "homepage"
-//   },
-// }
-// mvcCore.app
-//   .get('*', (req, res) => 
-//   {
-//       var mUrl = req.originalUrl;
-//       mvcRouterModule.DecodeUrl(mUrl).then(function(result){
-//         mUrl = result;
-//         var controllerStObj = mvcCore.listMapRoute[mUrl];
-//         if(controllerStObj == undefined){
-//           res.status(404);
-//           res.render('404', { url: req.url});
-//           return;
-//         }
-        
-//         var controllerName = controllerStObj.controllerName;
-//         var action = controllerStObj.action;
-//         var controllerObj = mvcCore.GetListControllers()[controllerName];
-//         var instance = new controllerObj();
-        
-//         instance[action]().then(function(result){
-//           res.render(result.viewPath, {ViewData : result.ViewData});
-//         });
-//       }, function(error){
-//           res.status(404);
-//           res.render('404', { url: req.url});
-//           return;
-//       });
-//   })
-//   .listen(PORT, function(){
-//     mvcCore.CurrentRootDir = __dirname;
-//     mvcCore.ControllerPath = 'controllers';
-//     mvcCore.ViewPath = __dirname + "/views";
-//     mvcCore.InitControllers().then(function(result){
-
-//     }, function(error){
-
-//     });
-//   });
-
 mvcCore.app.use(mvcCore.express.static(path.join(__dirname, 'public')));
   mvcCore.SetViewEnginee('ejs').then(function (data){
   mvcCore.SetViewDirectory(path.join(__dirname, 'views')).then(function(res){
-    mvcCore.listMapRoute = {
-      "/home" : {
-        controllerName : "home",
-        action: "home2"
-      },
-    };
-    mvcCore.CurrentRootDir = __dirname;
-    mvcCore.ControllerPath = 'controllers';
-    mvcCore.ViewPath = __dirname + "/views";
-      mvcCore.InitControllers().then(function(res2){
-      mvcCore.InitRequestRoute().then(function(res3){
-        mvcCore.startServer(PORT);
+    mvcCore.LoadFileMapRoute('./lib/MViC/configs/router_map.cfg').then(function(res2){
+      mvcCore.CurrentRootDir = __dirname;
+      mvcCore.ControllerPath = 'controllers';
+      mvcCore.ViewPath = __dirname + "/views";
+        mvcCore.InitControllers().then(function(res3){
+        mvcCore.InitRequestRoute().then(function(res4){
+          mvcCore.startServer(PORT);
+        });
       });
     });
   });
