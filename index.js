6ff8471492f8529945579e97c486a2359d5b110f
Le Phuc <phucleo093@gmail.com>
2020-12-13 11:25:46 +0700

    Test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const PORT = process.env.PORT || 5000
 const mvcCore = require('./lib/MViC/components/MVCCore');
+var mvcConst = require('./lib/MViC/components/MVCConst');
 
 mvcCore.app.use(mvcCore.express.static(path.join(__dirname, 'public')));
   mvcCore.SetViewEnginee('ejs').then(function (data){
@@ -9,6 +10,7 @@ mvcCore.app.use(mvcCore.express.static(path.join(__dirname, 'public')));
       mvcCore.CurrentRootDir = __dirname;
       mvcCore.ControllerPath = 'controllers';
       mvcCore.ViewPath = __dirname + "/views";
+      mvcConst.ViewPath = __dirname + "/views";
         mvcCore.InitControllers().then(function(res3){
         mvcCore.InitRequestRoute().then(function(res4){
           mvcCore.startServer(PORT);
